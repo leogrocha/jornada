28391405ee66f47fae78de34a212409d4e5e8cea
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-07-06 19:34:31 -0300

    Adicionando entidade Depoimento e testando criação da tabela no banco teste h2

---
+++ src/main/java/com/challenge/jornada/entities/Depoimento.java
@@ -0,0 +1,34 @@
+package com.challenge.jornada.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table
+@ToString
+@EqualsAndHashCode(of = { "idDepoimento" })
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Depoimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE) private Long idDepoimento;
+    @Column(columnDefinition = "TEXT") private String foto;
+    @Column(columnDefinition = "TEXT") private String depoimento;
+    private String nome;
+
+}

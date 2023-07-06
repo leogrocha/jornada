d02531f1c7b532e7728de802e23c67dd41422efe
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-07-06 19:19:22 -0300

    Initial commit

---
+++ src/main/java/com/challenge/jornada/JornadaApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.jornada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/jornada/JornadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.jornada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

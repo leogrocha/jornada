864f8e13bdcf4a647e5c388085aa2bde750a4180
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-07-25 23:01:32 -0300

    Voltando query manual como estava anteriormente

--- src/main/java/com/challenge/jornada/repositories/DestinoRepository.java
+++ src/main/java/com/challenge/jornada/repositories/DestinoRepository.java
@@ -3,14 +3,14 @@ package com.challenge.jornada.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
 
 import com.challenge.jornada.entities.Destino;
 
 public interface DestinoRepository extends JpaRepository<Destino, Long> {
 
-    // @Query(nativeQuery = true, value = "SELECT * FROM TB_DESTINO WHERE nome ilike %:nome%")
-    // List<Destino> searchByList(String nome);
+    @Query(nativeQuery = true, value = "SELECT * FROM TB_DESTINO WHERE nome ilike %:nome%")
+    List<Destino> searchByList(String nome);
 
-    List<Destino> findByNomeIlikDestinos(String nome);
+    // List<Destino> findByNomeIlikDestinos(String nome);
 }
